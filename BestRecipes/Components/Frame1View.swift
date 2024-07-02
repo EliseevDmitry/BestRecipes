482cfa621d83d94bbd3ef309157a7e5e3ac5c2e9
SnakCat <snakkdss@gmail.com>
2024-07-02 16:07:37 +0300

    created Frame1View

--- BestRecipes/Components/Frame1View.swift
+++ BestRecipes/Components/Frame1View.swift
@@ -9,7 +9,78 @@ import SwiftUI
 
 struct Frame1View: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            // общий стек
+            ZStack {
+                VStack(alignment: .leading) {
+                    // стек с видео
+                    ZStack {
+                        Image("mockImage1")
+                            .frame(height: 180)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                        // вложеные вью на картнинку
+                        VStack {
+                            HStack {
+                                // оценка
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .fill(.secondary)
+                                        .frame(width: 58, height: 28)
+                                    HStack {
+                                        Image(systemName: "star.fill")
+                                            .frame(width: 16, height: 16)
+                                        Text("5,0")
+                                            .font(.custom(Poppins.Bold, size: 14))
+                                            .foregroundStyle(.white)
+                                    }
+                                }
+                                Spacer()
+                                // бук марк
+                                ZStack {
+                                    Circle()
+                                        .frame(width: 32, height: 32)
+                                        .foregroundStyle(.white)
+                                    Image("iconeBookmark")
+                                }
+                            }
+                            .padding(.vertical, 8)
+                            .padding(.horizontal, 8)
+                            Spacer()
+                            HStack {
+                                Spacer()
+                                // время
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .fill(.secondary)
+                                        .frame(width: 41, height: 25)
+                                    Text("15:10")
+                                        .font(.custom(Poppins.Regular, size: 12))
+                                        .foregroundStyle(.white)
+                                }
+                            }
+                            .padding(.vertical, 8)
+                            .padding(.horizontal, 8)
+                        }
+                    }
+                    // стек под видео
+                    VStack(alignment: .leading) {
+                        Text("How to make sharwama at home")
+                            .font(.custom(Poppins.Bold, size: 16))
+                            .padding(.vertical, 12)
+                        HStack {
+                            Image("mockAvatar")
+                                .resizable()
+                                .frame(width: 32, height: 32)
+                                .clipShape(Circle())
+                            Text("By Zellicous Foods")
+                                .font(.custom(Poppins.Regular, size: 12))
+                                .foregroundStyle(.secondary)
+                        }
+                    }
+                }
+            }
+            .padding(.horizontal, 24)
+        }
     }
 }
 
