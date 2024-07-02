6fcbf3cf6df5349fc915a1e9098eecbb4e34f2f2
Dmitry <dimasel@gmail.com>
2024-07-02 23:11:07 +0700

    Colors, NavBarShape, OnboardingStorage

--- BestRecipes/Components/SearchView.swift
+++ BestRecipes/Components/SearchView.swift
@@ -9,21 +9,7 @@ import SwiftUI
 
 struct SearchView: View {
     var body: some View {
-        GeometryReader { screen in
-            
-            Path() {path in
-                path.move(to: CGPoint(x: 0, y: 0))
-                path.addLine(to: CGPoint(x: 0, y: 100))
-                path.addLine(to: CGPoint(x: screen.size.width, y: 100))
-                path.addLine(to: CGPoint(x: screen.size.width, y: 0))
-                path.addLine(to: CGPoint(x: screen.size.width/2+100, y: 0))
-                path.addQuadCurve(to: CGPoint(x: screen.size.width/2-100, y: 0), control: CGPoint(x: screen.size.width/2, y: 0))
-                path.closeSubpath()
-                
-             
-            }
-            .fill(.red)
-        }
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
