3b06c6ece2d4572fcb1e23170dda3de1350131a7
denisgindulin <denisgindulin@me.com>
2024-07-14 10:15:07 +0300

    refactor the SearchCuisines screen - change a system navigation bar to the custom navigation bar

--- BestRecipes/Views/SearchCuisines.swift
+++ BestRecipes/Views/SearchCuisines.swift
@@ -13,39 +13,60 @@ struct SearchCuisines: View {
     @State private var searchResults: [SearchResultRecipe] = []
     //@Binding var trendingItems: [Frame1View]
     var cuisines: String
+    @Environment(\.dismiss) var dismiss
+    @State private var showAlert = false
     @State private var errorMessage: String?
     @State private var isLoading = false
     var body: some View {
-        NavigationView{
-            VStack{
-                ScrollView(.vertical, showsIndicators: false) {
-                    VStack{
-                        HStack{
-                            Text("Trending now")
-                                .font(.custom(Poppins.bold, size: 24))
-                            Spacer()
-                        }
+        VStack{
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack{
+                    HStack{
+                        Text("Trending now")
+                            .font(.custom(Poppins.bold, size: 24))
+                        Spacer()
                     }
-                    .padding(.horizontal, 20)
-                    .padding(.top, 70)
-                    LazyVStack {
-                        ForEach(searchResults, id: \.id) { item in
-                            NavigationLink(destination: RecipeDetailView(appManager: appManager, recipeId: item.id)) {
-                                Frame1View(appManager: appManager, id: item.id ?? 1, foodFoto: item.image ?? "", title: item.title ?? "", cuisines: [cuisines])
-                                    .padding(.horizontal)
-                                    .padding(.bottom, 20)
-                            }
-                        }
-                    }
-                    .padding(.bottom, 80)
                 }
+                .padding(.horizontal, 20)
+                .padding(.top, 100)
+                LazyVStack {
+                    ForEach(searchResults, id: \.id) { item in
+                        NavigationLink(destination: RecipeDetailView(appManager: appManager, recipeId: item.id)) {
+                            Frame1View(appManager: appManager, id: item.id ?? 1, foodFoto: item.image ?? "", title: item.title ?? "", cuisines: [cuisines])
+                                .padding(.horizontal)
+                                .padding(.bottom, 20)
+                        }
+                    }
+                }
+                .padding(.bottom, 80)
             }
-            .ignoresSafeArea(.all, edges: .all)
         }
+        .ignoresSafeArea(.all, edges: .all)
         .task {
             searchCuisines(cuisines: cuisines)
         }
-        
+        // в панели навигации: стрелка влево вместо кнопки Back и кнопка "...", вызывающая alert
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button {
+                    dismiss()
+                } label: {
+                    Image(systemName: "arrow.left")
+                        .foregroundStyle(.primary)
+                }
+            }
+            ToolbarItem(placement: .topBarTrailing) {
+                Button {
+                    showAlert = true
+                } label: {
+                    Image(systemName: "ellipsis")
+                }
+            }
+        }
+        .alert(isPresented: $showAlert) {
+            Alert(title: Text("- Разыгрался аппетит?"), message: Text("- Да, согласен даже перевести 100 рублей разработчикам!"))
+        }
     }
     
     func searchCuisines(cuisines: String){
@@ -65,5 +86,4 @@ struct SearchCuisines: View {
             }
         }
     }
-    
 }
